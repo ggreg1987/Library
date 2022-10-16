0a707f14bb4b1934c39cc29041efb3a7240baaa0
greg <gabriel.gregorio1987@gmail.com>
2022-10-16 10:45:38 -0300

    Creating the BookController

---
+++ Library/src/main/java/io/github/ggreg1987/Library/domain/rest/controller/BookController.java
@@ -0,0 +1,9 @@
+package io.github.ggreg1987.Library.domain.rest.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+}
