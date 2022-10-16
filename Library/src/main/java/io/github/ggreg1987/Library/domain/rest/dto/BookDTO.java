d11a48fca3c8ff5f2dc3575130a4d442436a6024
greg <gabriel.gregorio1987@gmail.com>
2022-10-16 10:48:41 -0300

    Creating BookDTO class

---
+++ Library/src/main/java/io/github/ggreg1987/Library/domain/rest/dto/BookDTO.java
@@ -0,0 +1,17 @@
+package io.github.ggreg1987.Library.domain.rest.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookDTO {
+
+    private Long id;
+    private String title;
+    private String author;
+    private String isbn;
+}
