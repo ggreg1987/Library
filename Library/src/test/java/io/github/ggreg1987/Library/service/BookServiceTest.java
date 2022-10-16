118f5f5fe43f8e1fdb6851552d89bafcc884cf10
greg <gabriel.gregorio1987@gmail.com>
2022-10-16 14:33:57 -0300

    Creating BookServiceTest for unit test

---
+++ Library/src/test/java/io/github/ggreg1987/Library/service/BookServiceTest.java
@@ -0,0 +1,12 @@
+package io.github.ggreg1987.Library.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+public class BookServiceTest {
+
+    
+}
