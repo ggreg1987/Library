188cca5ab7cf2769bf3f5eb9bc531e42bad74a5d
greg <gabriel.gregorio1987@gmail.com>
2022-10-16 14:57:15 -0300

    Creating a BookReporitory interface

---
+++ Library/src/main/java/io/github/ggreg1987/Library/domain/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package io.github.ggreg1987.Library.domain.repository;
+
+import io.github.ggreg1987.Library.domain.entities.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book,Long> {
+}
--- Library/src/main/java/io/github/ggreg1987/Library/domain/rest/service/impl/BookServiceImpl.java
+++ Library/src/main/java/io/github/ggreg1987/Library/domain/rest/service/impl/BookServiceImpl.java
@@ -1,6 +1,7 @@
 package io.github.ggreg1987.Library.domain.rest.service.impl;
 
 import io.github.ggreg1987.Library.domain.entities.Book;
+import io.github.ggreg1987.Library.domain.repository.BookRepository;
 import io.github.ggreg1987.Library.domain.rest.service.BookService;
 import org.springframework.stereotype.Service;
 
@@ -8,7 +9,7 @@ import org.springframework.stereotype.Service;
 public class BookServiceImpl implements BookService {
 
 
-    private BookReporitory repository;
+    private BookRepository repository;
 
     @Override
     public Book save(Book book) {
