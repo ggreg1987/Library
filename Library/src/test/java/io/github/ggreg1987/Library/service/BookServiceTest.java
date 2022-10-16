ea13570ec94105c4c2a0efa0f34c54318715c379
greg <gabriel.gregorio1987@gmail.com>
2022-10-16 14:37:22 -0300

    Creating and saving a book

--- Library/src/test/java/io/github/ggreg1987/Library/service/BookServiceTest.java
+++ Library/src/test/java/io/github/ggreg1987/Library/service/BookServiceTest.java
@@ -1,5 +1,9 @@
 package io.github.ggreg1987.Library.service;
 
+import io.github.ggreg1987.Library.domain.entities.Book;
+import io.github.ggreg1987.Library.domain.rest.service.BookService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -8,5 +12,21 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ActiveProfiles("test")
 public class BookServiceTest {
 
-    
+    BookService service;
+
+    @Test
+    @DisplayName("Should save a book.")
+    public void saveBookTest() {
+
+        Book book = Book
+                .builder()
+                .author("Gregorio")
+                .title("Somethings")
+                .isbn("54321")
+                .build();
+        var savedBook = service.save(book);
+
+        
+
+    }
 }
