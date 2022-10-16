713990fa1959545f425510ece380874070927519
greg <gabriel.gregorio1987@gmail.com>
2022-10-16 10:04:05 -0300

    Injecting MockMvc to requests simulate

--- Library/src/test/java/io/github/ggreg1987/Library/controller/BookControllerTest.java
+++ Library/src/test/java/io/github/ggreg1987/Library/controller/BookControllerTest.java
@@ -1,14 +1,19 @@
 package io.github.ggreg1987.Library.controller;
 
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
 @WebMvcTest
 @AutoConfigureMockMvc
 public class BookControllerTest {
+
+    @Autowired
+    MockMvc mvc;
 }
