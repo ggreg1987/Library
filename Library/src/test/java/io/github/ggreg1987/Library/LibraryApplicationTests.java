8b5699852f1b83ae41638778932ad2489b40f3b5
greg <gabriel.gregorio1987@gmail.com>
2022-10-16 09:21:15 -0300

    Starting the Project

---
+++ Library/src/test/java/io/github/ggreg1987/Library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ggreg1987.Library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
