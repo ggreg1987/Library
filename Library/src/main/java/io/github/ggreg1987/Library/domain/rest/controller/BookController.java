013a92aa2c7e1dfd108e181f09a87d4a539caa64
greg <gabriel.gregorio1987@gmail.com>
2022-10-16 10:46:59 -0300

    Creating a post method to create a book

--- Library/src/main/java/io/github/ggreg1987/Library/domain/rest/controller/BookController.java
+++ Library/src/main/java/io/github/ggreg1987/Library/domain/rest/controller/BookController.java
@@ -1,9 +1,16 @@
 package io.github.ggreg1987.Library.domain.rest.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/books")
 public class BookController {
+
+    @PostMapping
+    public BookDTO create(@RequestBody BookDTO dto) {
+
+    }
 }
