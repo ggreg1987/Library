143526169b2ae34ae8c089588a8da1a38a9e89e5
greg <gabriel.gregorio1987@gmail.com>
2022-10-16 13:52:31 -0300

    Simulating the save method

--- Library/src/test/java/io/github/ggreg1987/Library/controller/BookControllerTest.java
+++ Library/src/test/java/io/github/ggreg1987/Library/controller/BookControllerTest.java
@@ -7,6 +7,8 @@ import io.github.ggreg1987.Library.domain.rest.service.BookService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -47,6 +49,7 @@ public class BookControllerTest {
                 .isbn("12345")
                 .build();
 
+        BDDMockito.given(service.save(Mockito.any(Book.class))).willReturn(null);
         String json = new ObjectMapper().writeValueAsString(dto);
 
         MockHttpServletRequestBuilder request = MockMvcRequestBuilders
