0a4726f2bcfb8ded4b3cddd45033d0c7e4d15bae
greg <gabriel.gregorio1987@gmail.com>
2022-10-16 15:00:48 -0300

    Mocking BookRepository

--- Library/src/main/java/io/github/ggreg1987/Library/domain/rest/service/impl/BookServiceImpl.java
+++ Library/src/main/java/io/github/ggreg1987/Library/domain/rest/service/impl/BookServiceImpl.java
@@ -3,13 +3,15 @@ package io.github.ggreg1987.Library.domain.rest.service.impl;
 import io.github.ggreg1987.Library.domain.entities.Book;
 import io.github.ggreg1987.Library.domain.repository.BookRepository;
 import io.github.ggreg1987.Library.domain.rest.service.BookService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class BookServiceImpl implements BookService {
 
 
-    private BookRepository repository;
+    private final BookRepository repository;
 
     @Override
     public Book save(Book book) {
--- Library/src/test/java/io/github/ggreg1987/Library/service/BookServiceTest.java
+++ Library/src/test/java/io/github/ggreg1987/Library/service/BookServiceTest.java
@@ -1,12 +1,14 @@
 package io.github.ggreg1987.Library.service;
 
 import io.github.ggreg1987.Library.domain.entities.Book;
+import io.github.ggreg1987.Library.domain.repository.BookRepository;
 import io.github.ggreg1987.Library.domain.rest.service.BookService;
 import io.github.ggreg1987.Library.domain.rest.service.impl.BookServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -16,10 +18,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class BookServiceTest {
 
     BookService service;
+    @MockBean
+    BookRepository repository;
 
     @BeforeEach
     public void setUp() {
-        this.service = new BookServiceImpl();
+        this.service = new BookServiceImpl(repository);
     }
 
     @Test
