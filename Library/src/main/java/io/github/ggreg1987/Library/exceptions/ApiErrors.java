eb9fd005be16ca1de49b9d72a9268c6c1a46f242
greg <gabriel.gregorio1987@gmail.com>
2022-10-17 14:31:13 -0300

    Adding Lombok Getter!

--- Library/src/main/java/io/github/ggreg1987/Library/exceptions/ApiErrors.java
+++ Library/src/main/java/io/github/ggreg1987/Library/exceptions/ApiErrors.java
@@ -1,10 +1,11 @@
 package io.github.ggreg1987.Library.exceptions;
 
+import lombok.Getter;
 import org.springframework.validation.BindingResult;
 
 import java.util.ArrayList;
 import java.util.List;
-
+@Getter
 public class ApiErrors {
 
     private List<String> errors;
