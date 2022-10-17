eb83a2900fc9b9a556b5903122d16b4f70324adb
greg <gabriel.gregorio1987@gmail.com>
2022-10-17 15:13:01 -0300

    Creating a new constructor for BusinessException

--- Library/src/main/java/io/github/ggreg1987/Library/exceptions/ApiErrors.java
+++ Library/src/main/java/io/github/ggreg1987/Library/exceptions/ApiErrors.java
@@ -1,9 +1,11 @@
 package io.github.ggreg1987.Library.exceptions;
 
+import io.github.ggreg1987.Library.businessRule.BusinessException;
 import lombok.Getter;
 import org.springframework.validation.BindingResult;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 @Getter
 public class ApiErrors {
@@ -13,5 +15,9 @@ public class ApiErrors {
         this.errors = new ArrayList<>();
         bindingResult.getAllErrors().forEach(error ->
                 this.errors.add(error.getDefaultMessage()));
+    }
+
+    public ApiErrors(BusinessException ex) {
+        this.errors = Arrays.asList(ex.getMessage());
     }
 }
