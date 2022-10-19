c9e7fcd3b739131dbba6a795ca10c3d48a946b52
greg <gabriel.gregorio1987@gmail.com>
2022-10-19 12:39:12 -0300

    Mapping book with JPA

--- Library/src/main/java/io/github/ggreg1987/Library/domain/entities/Book.java
+++ Library/src/main/java/io/github/ggreg1987/Library/domain/entities/Book.java
@@ -2,14 +2,24 @@ package io.github.ggreg1987.Library.domain.entities;
 
 import lombok.*;
 
+import javax.persistence.*;
+
 @Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Entity
+@Table(name = "book")
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(length = 50,nullable = false)
     private String author;
+    @Column(length = 100,nullable = false)
     private String title;
+    @Column(length = 10,nullable = false)
     private String isbn;
 }
