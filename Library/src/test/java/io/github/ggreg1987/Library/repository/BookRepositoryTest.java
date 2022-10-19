20cbcaf86a5d7b4cf49e496616ce3ae023ce5421
greg <gabriel.gregorio1987@gmail.com>
2022-10-19 11:57:44 -0300

    Injecting EntityManagerTest object

--- Library/src/test/java/io/github/ggreg1987/Library/repository/BookRepositoryTest.java
+++ Library/src/test/java/io/github/ggreg1987/Library/repository/BookRepositoryTest.java
@@ -1,7 +1,9 @@
 package io.github.ggreg1987.Library.repository;
 
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -9,4 +11,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ActiveProfiles("test")
 @DataJpaTest
 public class BookRepositoryTest {
+
+    @Autowired
+    TestEntityManager testEntityManager;
 }
