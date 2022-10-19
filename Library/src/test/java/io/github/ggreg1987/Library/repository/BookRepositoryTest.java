b16a842f77b74f69cbcef55ac59718ea047d57bb
greg <gabriel.gregorio1987@gmail.com>
2022-10-19 12:22:22 -0300

    Creating a method for checking if exists a book with isbn informed

--- Library/src/test/java/io/github/ggreg1987/Library/repository/BookRepositoryTest.java
+++ Library/src/test/java/io/github/ggreg1987/Library/repository/BookRepositoryTest.java
@@ -1,5 +1,8 @@
 package io.github.ggreg1987.Library.repository;
 
+import io.github.ggreg1987.Library.domain.repository.BookRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -14,4 +17,13 @@ public class BookRepositoryTest {
 
     @Autowired
     TestEntityManager testEntityManager;
+
+    @Autowired
+    BookRepository repository;
+
+    @Test
+    @DisplayName("Should return true when exists a book with isbn informed.")
+    public void returnTrueWhenIsbExists() {
+
+    }
 }
