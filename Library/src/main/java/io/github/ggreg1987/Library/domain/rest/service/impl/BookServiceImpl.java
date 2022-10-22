b040e25c0441217fdbfb5dfb438e854242d5f24e
greg <gabriel.gregorio1987@gmail.com>
2022-10-21 22:25:16 -0300

    Implemanteting delete method

--- Library/src/main/java/io/github/ggreg1987/Library/domain/rest/service/impl/BookServiceImpl.java
+++ Library/src/main/java/io/github/ggreg1987/Library/domain/rest/service/impl/BookServiceImpl.java
@@ -30,8 +30,11 @@ public class BookServiceImpl implements BookService {
     }
 
     @Override
-    public void delete(Book book) {
-
+    public void delete(Book book)  {
+        if(book == null || book.getId() == null) {
+            throw new IllegalArgumentException("Book id cant be null");
+        }
+        repository.delete(book);
     }
 
     @Override
