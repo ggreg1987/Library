24907876c44e439f6de507abb78164346b694ab1
greg <gabriel.gregorio1987@gmail.com>
2022-10-23 18:41:34 -0300

    Creating endpoint to find a book by name and title

--- Library/src/main/java/io/github/ggreg1987/Library/domain/rest/controller/BookController.java
+++ Library/src/main/java/io/github/ggreg1987/Library/domain/rest/controller/BookController.java
@@ -7,6 +7,9 @@ import io.github.ggreg1987.Library.domain.rest.service.BookService;
 import io.github.ggreg1987.Library.exceptions.ApiErrors;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -14,6 +17,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.springframework.http.HttpStatus.*;
 
@@ -58,6 +64,19 @@ public class BookController {
                     return modelMapper.map(book,BookDTO.class);
                 })
                 .orElseThrow(() -> new ResponseStatusException(NOT_FOUND));
+    }
+
+    @GetMapping
+    public Page<BookDTO> find(BookDTO dto , Pageable pageRequest) {
+        var filter = modelMapper.map(dto,Book.class);
+        Page<Book> result = service.find(filter, pageRequest);
+        List<BookDTO> list = result.getContent()
+                .stream()
+                .map(entity -> modelMapper.map(entity, BookDTO.class))
+                .collect(Collectors.toList());
+
+        return new PageImpl<BookDTO>(list,pageRequest,result.getTotalElements());
+
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
