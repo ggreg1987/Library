33c1249664f74e3c0c446ed27350211d99e6d0a8
greg <gabriel.gregorio1987@gmail.com>
2022-10-24 15:35:16 -0300

    Configuring to upload the api only with BookControllerTest

--- Library/src/test/java/io/github/ggreg1987/Library/controller/BookControllerTest.java
+++ Library/src/test/java/io/github/ggreg1987/Library/controller/BookControllerTest.java
@@ -34,7 +34,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
-@WebMvcTest
+@WebMvcTest(controllers = BookControllerTest.class)
 @AutoConfigureMockMvc
 public class BookControllerTest {
 
