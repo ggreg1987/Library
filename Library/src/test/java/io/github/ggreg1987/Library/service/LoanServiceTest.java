c91f57d6e17b59450f3055e3ce152ea124c13cc6
greg <gabriel.gregorio1987@gmail.com>
2022-10-25 14:44:52 -0300

    Testing the return

--- Library/src/test/java/io/github/ggreg1987/Library/service/LoanServiceTest.java
+++ Library/src/test/java/io/github/ggreg1987/Library/service/LoanServiceTest.java
@@ -1,11 +1,22 @@
 package io.github.ggreg1987.Library.service;
 
+import io.github.ggreg1987.Library.domain.entities.Book;
+import io.github.ggreg1987.Library.domain.entities.Loan;
 import io.github.ggreg1987.Library.domain.repository.LoanRepository;
 import io.github.ggreg1987.Library.domain.rest.service.LoanService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
@@ -16,5 +27,27 @@ public class LoanServiceTest {
     @MockBean
     LoanRepository loanRepository;
 
+    @Test
+    @DisplayName("Should save a loan.")
+    public void saveLoanTest() {
 
+        var book = Book.builder().id(1L)
+                .author("Gregorio")
+                .title("The Avengers").isbn("12345").build();
+
+        var loan = Loan.builder()
+                .customer("Gabriel")
+                .book(book).loanDate(LocalDate.now()).build();
+        var loanReturn = Loan.builder()
+                .id(1L).customer("Gabriel")
+                .book(book).loanDate(LocalDate.now()).build();
+
+        when(loanRepository.save(loan)).thenReturn(loanReturn);
+
+        var loanSaved = service.save(loan);
+
+        assertThat(loanSaved.getId()).isEqualTo(loanReturn.getId());
+        assertThat(loanSaved.getBook().getId()).isEqualTo(loanReturn.getBook().getId());
+        assertThat(loanSaved.getBook().getIsbn()).isEqualTo(loanReturn.getBook().getIsbn());
+    }
 }
