42bb356a5afa8fc4645161c62f20800fdfdbb753
greg <gabriel.gregorio1987@gmail.com>
2022-10-26 19:18:59 -0300

    Adding the an exception

--- Library/src/main/java/io/github/ggreg1987/Library/domain/rest/service/impl/LoanServiceImpl.java
+++ Library/src/main/java/io/github/ggreg1987/Library/domain/rest/service/impl/LoanServiceImpl.java
@@ -1,5 +1,6 @@
 package io.github.ggreg1987.Library.domain.rest.service.impl;
 
+import io.github.ggreg1987.Library.businessRule.BusinessException;
 import io.github.ggreg1987.Library.domain.entities.Loan;
 import io.github.ggreg1987.Library.domain.repository.LoanRepository;
 import io.github.ggreg1987.Library.domain.rest.service.LoanService;
@@ -14,8 +15,8 @@ public class LoanServiceImpl implements LoanService {
     @Override
     public Loan save(Loan loan) {
         if(repository.existsByBookAndNotReturned(loan.getBook())) {
-
+            throw new BusinessException("Book already loaned.");
         }
-        return repository.save(any);
+        return repository.save(loan);
     }
 }
