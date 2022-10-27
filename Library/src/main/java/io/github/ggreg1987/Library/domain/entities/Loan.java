5edde38b5235997babea9b7930e88cbc858e315b
greg <gabriel.gregorio1987@gmail.com>
2022-10-27 16:13:56 -0300

    Mapping the Loan entity with Jpa

--- Library/src/main/java/io/github/ggreg1987/Library/domain/entities/Loan.java
+++ Library/src/main/java/io/github/ggreg1987/Library/domain/entities/Loan.java
@@ -2,21 +2,25 @@ package io.github.ggreg1987.Library.domain.entities;
 
 import lombok.*;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Entity
 public class Loan {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(length = 100)
     private String customer;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id")
     private Book book;
     private LocalDate loanDate;
     private Boolean returned;
