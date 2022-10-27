32702ad1cc367f252788b888e2d0c014eee4b658
greg <gabriel.gregorio1987@gmail.com>
2022-10-27 15:27:50 -0300

    Creating a new method to verifying if there is an unreturned loan

--- Library/src/test/java/io/github/ggreg1987/Library/repository/LoanRepositoryTest.java
+++ Library/src/test/java/io/github/ggreg1987/Library/repository/LoanRepositoryTest.java
@@ -1,5 +1,7 @@
 package io.github.ggreg1987.Library.repository;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -10,5 +12,11 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @DataJpaTest
 public class LoanRepositoryTest {
 
-    
+    @Test
+    @DisplayName("Should verifying if there is an unreturned loan")
+    public void existsByBookAndNotReturnedTest() {
+
+        
+
+    }
 }
