c309f402108df5766e6e1b6b4a7b7725e471677e
greg <gabriel.gregorio1987@gmail.com>
2022-10-27 16:28:53 -0300

    Fixing an error with custom Query using JPQL

--- Library/src/main/java/io/github/ggreg1987/Library/domain/repository/LoanRepository.java
+++ Library/src/main/java/io/github/ggreg1987/Library/domain/repository/LoanRepository.java
@@ -3,7 +3,12 @@ package io.github.ggreg1987.Library.domain.repository;
 import io.github.ggreg1987.Library.domain.entities.Book;
 import io.github.ggreg1987.Library.domain.entities.Loan;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface LoanRepository extends JpaRepository<Loan,Long> {
-    boolean existsByBookAndNotReturned(Book book);
+    @Query(value = "SELECT CASE WHEN ( COUNT(l.id) > 0 ) " +
+            "THEN TRUE ELSE FALSE END FROM Loan l WHERE l.book = :book " +
+            "AND (l.returned IS NULL OR l.returned IS FALSE)")
+    boolean existsByBookAndNotReturned(@Param("book") Book book);
 }
