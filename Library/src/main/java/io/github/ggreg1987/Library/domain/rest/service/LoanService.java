81ef1965ede390323529da205719a15ca8b798aa
greg <gabriel.gregorio1987@gmail.com>
2022-10-27 22:24:39 -0300

    Creating two signatures, getById and update

--- Library/src/main/java/io/github/ggreg1987/Library/domain/rest/service/LoanService.java
+++ Library/src/main/java/io/github/ggreg1987/Library/domain/rest/service/LoanService.java
@@ -2,6 +2,13 @@ package io.github.ggreg1987.Library.domain.rest.service;
 
 import io.github.ggreg1987.Library.domain.entities.Loan;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface LoanService {
     Loan save(Loan any);
+
+    Optional<Loan> getById(Long id);
+
+    Loan update(Loan loan);
 }
