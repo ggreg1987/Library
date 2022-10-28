4149f6b25c9d50cc9d84831b6b9fe85570127b3d
greg <gabriel.gregorio1987@gmail.com>
2022-10-27 22:05:51 -0300

    Creating a ReturnedLoanDTO class

---
+++ Library/src/main/java/io/github/ggreg1987/Library/domain/rest/dto/ReturnedLoanDTO.java
@@ -0,0 +1,12 @@
+package io.github.ggreg1987.Library.domain.rest.dto;
+
+import lombok.*;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ReturnedLoanDTO {
+
+    private Boolean returned;
+}
