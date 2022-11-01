a5003390d3e7b585d468796c8c4c40e41723a502
greg <gabriel.gregorio1987@gmail.com>
2022-11-01 18:20:29 -0300

    Creating a new method in LoanRepository

--- Library/src/main/java/io/github/ggreg1987/Library/domain/repository/LoanRepository.java
+++ Library/src/main/java/io/github/ggreg1987/Library/domain/repository/LoanRepository.java
@@ -2,6 +2,8 @@ package io.github.ggreg1987.Library.domain.repository;
 
 import io.github.ggreg1987.Library.domain.entities.Book;
 import io.github.ggreg1987.Library.domain.entities.Loan;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,4 +13,6 @@ public interface LoanRepository extends JpaRepository<Loan,Long> {
             "THEN TRUE ELSE FALSE END FROM Loan l WHERE l.book = :book " +
             "AND (l.returned IS NULL OR l.returned IS FALSE)")
     boolean existsByBookAndNotReturned(@Param("book") Book book);
+
+    Page<Loan> findByBookIsbnOrCustomer(String isbn, String customer, PageRequest pageRequest);
 }
