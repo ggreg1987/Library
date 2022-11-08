22a173b2d1913496b57b16906dad2d495b225e5c
greg <gabriel.gregorio1987@gmail.com>
2022-11-08 17:24:05 -0300

    Creating a static Cron with 0 seconds,0 minutes, 0 hours, all days,any month, any year

--- Library/src/main/java/io/github/ggreg1987/Library/domain/rest/service/impl/ScheduleServiceImpl.java
+++ Library/src/main/java/io/github/ggreg1987/Library/domain/rest/service/impl/ScheduleServiceImpl.java
@@ -1,7 +1,10 @@
 package io.github.ggreg1987.Library.domain.rest.service.impl;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ScheduleServiceImpl {
+
+   private static final String CRON_LATE_LOANS = "0 0 0 1/1 * ?";
 }
