3f481a64c64cf811dff7809e9496a70997fe018d
greg <gabriel.gregorio1987@gmail.com>
2022-11-08 17:29:26 -0300

    Creating a new class and getting all late loans

--- Library/src/main/java/io/github/ggreg1987/Library/domain/rest/service/impl/ScheduleServiceImpl.java
+++ Library/src/main/java/io/github/ggreg1987/Library/domain/rest/service/impl/ScheduleServiceImpl.java
@@ -1,9 +1,12 @@
 package io.github.ggreg1987.Library.domain.rest.service.impl;
 
+import io.github.ggreg1987.Library.domain.entities.Loan;
 import io.github.ggreg1987.Library.domain.rest.service.LoanService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -13,4 +16,9 @@ public class ScheduleServiceImpl {
 
    private final LoanService loanService;
    private final EmailService emailService;
+
+   @Scheduled(cron = CRON_LATE_LOANS)
+   public void sendEmailToLateLoans() {
+       List<Loan> allLateLoans = loanService.getAllLateLoans();
+   }
 }
