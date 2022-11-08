9066fffa7c4523b249761271b8bf9804cebf4201
greg <gabriel.gregorio1987@gmail.com>
2022-11-08 17:27:06 -0300

    Injecting LoanService and EmailService, that last will be still create

--- Library/src/main/java/io/github/ggreg1987/Library/domain/rest/service/impl/ScheduleServiceImpl.java
+++ Library/src/main/java/io/github/ggreg1987/Library/domain/rest/service/impl/ScheduleServiceImpl.java
@@ -1,10 +1,16 @@
 package io.github.ggreg1987.Library.domain.rest.service.impl;
 
+import io.github.ggreg1987.Library.domain.rest.service.LoanService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ScheduleServiceImpl {
 
    private static final String CRON_LATE_LOANS = "0 0 0 1/1 * ?";
+
+   private final LoanService loanService;
+   private final EmailService emailService;
 }
