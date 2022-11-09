a8af43ea8998b1e73717d502d2ac63a97e5b5038
greg <gabriel.gregorio1987@gmail.com>
2022-11-09 03:16:09 -0300

    Creating a SwaggerConfig class

---
+++ Library/src/main/java/io/github/ggreg1987/Library/config/SwaggerConfig.java
@@ -0,0 +1,11 @@
+package io.github.ggreg1987.Library.config;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+
+    
+}
