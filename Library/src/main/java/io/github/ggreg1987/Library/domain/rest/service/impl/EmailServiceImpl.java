7c2ca06ddf7743fe31b829b67e30b04ab3f5bc54
greg <gabriel.gregorio1987@gmail.com>
2022-11-08 21:35:09 -0300

    Creating a key for default remitter and getting in string

--- Library/src/main/java/io/github/ggreg1987/Library/domain/rest/service/impl/EmailServiceImpl.java
+++ Library/src/main/java/io/github/ggreg1987/Library/domain/rest/service/impl/EmailServiceImpl.java
@@ -2,6 +2,7 @@ package io.github.ggreg1987.Library.domain.rest.service.impl;
 
 import io.github.ggreg1987.Library.domain.rest.service.EmailService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -12,10 +13,13 @@ import java.util.List;
 @RequiredArgsConstructor
 public class EmailServiceImpl implements EmailService {
 
+    @Value("${application.mail.default-remitter}")
+    private String  message;
+
     private final JavaMailSender javaMailSender;
     @Override
     public void sandMails(String message, List<String> mailList) {
         SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-        
+
     }
 }
