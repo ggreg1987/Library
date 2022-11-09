ed140af7cc12f9df208c9b9101e0a00ecb6f98c8
greg <gabriel.gregorio1987@gmail.com>
2022-11-08 21:31:15 -0300

    Stating SimpleMailMessage

--- Library/src/main/java/io/github/ggreg1987/Library/domain/rest/service/impl/EmailServiceImpl.java
+++ Library/src/main/java/io/github/ggreg1987/Library/domain/rest/service/impl/EmailServiceImpl.java
@@ -1,14 +1,21 @@
 package io.github.ggreg1987.Library.domain.rest.service.impl;
 
 import io.github.ggreg1987.Library.domain.rest.service.EmailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class EmailServiceImpl implements EmailService {
+
+    private final JavaMailSender javaMailSender;
     @Override
     public void sandMails(String message, List<String> mailList) {
-
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        
     }
 }
