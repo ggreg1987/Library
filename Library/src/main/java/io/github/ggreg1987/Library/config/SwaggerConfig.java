1cf956db0c6f1b99d2665756b16d1114a37508dc
greg <gabriel.gregorio1987@gmail.com>
2022-11-09 03:40:20 -0300

    Implemetating and setting the Docket method

--- Library/src/main/java/io/github/ggreg1987/Library/config/SwaggerConfig.java
+++ Library/src/main/java/io/github/ggreg1987/Library/config/SwaggerConfig.java
@@ -3,8 +3,11 @@ package io.github.ggreg1987.Library.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -14,7 +17,13 @@ public class SwaggerConfig {
 
     @Bean
     public Docket docket() {
-
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("package io.github.ggreg1987" +
+                        ".Library.domain.rest.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
     }
 
     private ApiInfo apiInfo() {
