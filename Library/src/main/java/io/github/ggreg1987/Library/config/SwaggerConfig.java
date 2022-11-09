2ef93d3a6b0ce764d88d171928285a6b74db203b
greg <gabriel.gregorio1987@gmail.com>
2022-11-09 03:27:55 -0300

    Creating 3 method where will be configurate the swagger

--- Library/src/main/java/io/github/ggreg1987/Library/config/SwaggerConfig.java
+++ Library/src/main/java/io/github/ggreg1987/Library/config/SwaggerConfig.java
@@ -1,11 +1,26 @@
 package io.github.ggreg1987.Library.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @Configuration
 public class SwaggerConfig {
 
-    
+    @Bean
+    public Docket docket() {
+
+    }
+
+    private ApiInfo apiInfo() {
+
+    }
+
+    private Contact contact() {
+        
+    }
 }
